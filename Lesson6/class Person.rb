d3ea89700491fc0ea7b4ed6aa029656e2105e633
GubatAbdullaev <Gubatabdullaev14@gmail.com>
2018-05-02 19:31:48 +0300

    First commit

---
+++ Lesson6/class Person.rb
@@ -0,0 +1,43 @@
+class Person
+attr_accessor :name, :age
+attr_reader :name, :age
+  def initialize(myname, myage)
+  @name = myname
+  self.age = myage
+end
+class Pupil < Person
+Anton = Person.new("7","Sviblovo")
+Anton.class = 7
+Anton.school = Sviblovo
+puts Anton.class
+puts Anton.school
+Vika = Person.new("7","1779")
+Vika.class = 7
+Vika.school = 1779
+puts Vika.class
+puts Vika.school
+Dima = Person.new("7","2018")
+Dima.class = 7
+Dima.school = 2018
+puts Dima.class
+puts Dima.school
+Diana = Person.new("7","1999")
+Diana.class = 7
+Diana.school = 1999
+puts Diana.class
+puts Diana.school
+Max = Person.new("7","2005")
+Max.class = 7
+Max.school = 2005
+puts Max.class
+puts Max.school
+class School
+attr_accessor :pupils
+  def initialize(school
+  @school = myschool
+  self.school = myschool
+end
+
+
+
+
